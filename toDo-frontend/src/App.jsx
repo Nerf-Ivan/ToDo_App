1299e73dbc9912235c1788914ed553d9da36aa19
Nerf-Ivan <ivan.swanepoel.dev@gmail.com>
2025-09-16 20:07:59 +0200

    Improved AddTask.jsx

--- toDo-frontend/src/App.jsx
+++ toDo-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx';
 import AddTask from './components/AddTask.jsx';
+import './index.css'
 
 export default function App() {
   return (
--- toDo-frontend/src/components/AddTask.jsx
+++ toDo-frontend/src/components/AddTask.jsx
@@ -1,23 +1,48 @@
 import '@fontsource/rubik-bubbles';
+import '@fontsource/lobster';
+import { CiCirclePlus } from "react-icons/ci";
+
 function AddTask(){
     return (
         <main>
-            <h1 style={{fontFamily: 'Rubik Bubbles, sans-serif'}} className="text-center mt-10 mb-10 text-2xl">Your To Do Items</h1>
+            <h1 
+              style={{fontFamily: 'Lobster, sans-serif'}} 
+              className="text-center mt-10 mb-10 text-2xl"
+            >
+              Your To Do Items
+            </h1>
             
-            <div className="flex items-center relative w-full max-w-md mx-auto md:max-w-lg lg:max-w-xl">
-  <input
-    className="border p-4 w-full focus:outline-none focus:ring-0 placeholder-gray-500 text-lg sm:p-8 md:p-10 lg:p-12"
-    placeholder="Add new task"
-  />
-  <button
-    className="absolute right-2 sm:right-3 md:right-4 text-black text-4xl sm:text-5xl md:text-6xl lg:text-7xl bg-blue-200 border rounded-2xl w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 flex items-center justify-center transform -translate-y-1/2 top-1/2"
-  >
-    +
-  </button>
-  <hr className="absolute bottom-7 left-0 w-full border-2 border-gray-500 rounded" />
-</div>
-            
+            <div className="flex items-center w-full max-w-md mx-auto md:max-w-lg lg:max-w-xl gap-2">
+              <input
+  type="text"
+  placeholder="Add new task"
+   className="
+    w-full 
+    border-0 
+    border-b-2 
+    border-gray-400 
+    focus:border-gray-300 
+    focus:outline-none 
+    focus:ring-0 
+    placeholder-gray-400 
+    placeholder:p-1
+    placeholder: italic
+    text-lg
+    bg-transparent
+    px-0
+    py-2
+    transition-colors 
+    duration-300
+  "
+/>
+              <button
+                className="w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-18 lg:h-18 flex items-center justify-center"
+              >
+                <CiCirclePlus className="text-gray-400 transition-colors duration-300 hover:text-white" size={40}/>
+              </button>
+            </div>
         </main>
     )
 }
-export default AddTask
+
+export default AddTask;
